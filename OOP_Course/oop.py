19ccfeb717a01c89c6afe941983083e1ce8ee35a
Cristian Prodius <m318572@alunos.ipb.pt>
2023-12-04 20:02:23 +0000

    solved the instace representation

--- OOP_Course/oop.py
+++ OOP_Course/oop.py
@@ -1,4 +1,7 @@
 class Item:
+    pay_rate = 0.8 #the pay after 20% discount
+    all = []
+
     def __init__(self, name: str, price: float, quantity=0) -> None:
         #Validate the reciving data
         assert price >= 0, f"Price {price} is not greater then zero"
@@ -8,11 +11,49 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        #Action to execute
+        Item.all.append(self)
+
     def CalculateTotalPrice(self):
         return self.price * self.quantity
+    
+    def ApplyDiscount(self):
+        self.price =self.price * self.pay_rate
+
+    def __repr__(self) -> str:
+        return f"Item('{self.name}','{self.price}','{self.quantity}')"
+
+
+"""
+Here we see how to apply a different discount to different items
 
 item1 = Item("Phone", 100, 5)
-item2 = Item("Laptop", 150,3)
+item1.ApplyDiscount()
+print(item1.price)
 
-print(item1.CalculateTotalPrice())
-print(item2.CalculateTotalPrice())
+item2 = Item("Laptop", 150,3)
+item2.pay_rate = 0.7 #change the discount for item 2 to 30% fut the item1 is still remain the same
+item2.ApplyDiscount()
+print(item2.price)
+"""
+
+
+
+"""
+The diference between atributes form class and instance level
+
+print(Item.__dict__) #print atributes for the class level
+print(item1.__dict__) #print atributes for the instance level
+"""
+
+item1 = Item("Phone", 100, 5)
+item2 = Item("Laptop", 150, 3)
+item2 = Item("TV", 1500, 1)
+item2 = Item("PC", 2050, 2)
+item2 = Item("Tablet", 250, 5)
+
+print(Item.all)
+
+for instance in Item.all:
+    print(instance.name)
