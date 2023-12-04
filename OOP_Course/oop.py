98dc5878df5043ea42b63d06b8abdb79c6442bdd
Cristian Prodius <m318572@alunos.ipb.pt>
2023-12-04 19:36:52 +0000

    validating the input data

--- OOP_Course/oop.py
+++ OOP_Course/oop.py
@@ -1,13 +1,18 @@
 class Item:
-    def __init__(self, name, price, quantity) -> None:
+    def __init__(self, name: str, price: float, quantity=0) -> None:
+        #Validate the reciving data
+        assert price >= 0, f"Price {price} is not greater then zero"
+        assert quantity >= 0, f"Quantity {quantity} is not greater then zero"
+
+        #Asing to self objecte
         self.name = name
         self.price = price
         self.quantity = quantity
-    def CalculateTotalPrice(self, x, y):
-        return x * y
+    def CalculateTotalPrice(self):
+        return self.price * self.quantity
 
 item1 = Item("Phone", 100, 5)
-print(item1.CalculateTotalPrice(item1.price, item1.quantity))
+item2 = Item("Laptop", 150,3)
 
-
-item2 = Item("Laptop", 100,3)
+print(item1.CalculateTotalPrice())
+print(item2.CalculateTotalPrice())
