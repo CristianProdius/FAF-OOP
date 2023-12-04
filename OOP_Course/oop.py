5d6734f9c3c411dd639474da2c8bc387ce30bf95
Cristian Prodius <m318572@alunos.ipb.pt>
2023-12-04 19:27:41 +0000

    geting rid of hardcode

--- OOP_Course/oop.py
+++ OOP_Course/oop.py
@@ -1,15 +1,13 @@
 class Item:
+    def __init__(self, name, price, quantity) -> None:
+        self.name = name
+        self.price = price
+        self.quantity = quantity
     def CalculateTotalPrice(self, x, y):
         return x * y
 
-item1 = Item()
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
+item1 = Item("Phone", 100, 5)
 print(item1.CalculateTotalPrice(item1.price, item1.quantity))
 
 
-item2 = Item()
-item2.name = "Laptop"
-item2.price = 100
-item2.quantity = 5
+item2 = Item("Laptop", 100,3)
